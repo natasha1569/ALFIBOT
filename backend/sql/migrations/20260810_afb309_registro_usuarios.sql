39757f2c598dfb02159065b9aeb68b34dfa91477
ArevaloRomel <rsarevalo@puce.edu.ec>
2026-08-11 18:18:20 -0500

    chore: [AFB] [Romel Arévalo] Retirar archivos SQL obsoletos

--- backend/sql/migrations/20260810_afb309_registro_usuarios.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    column_name,
-    data_type,
-    is_nullable,
-    column_default
-FROM information_schema.columns
-WHERE table_schema = 'alfi'
-  AND table_name = 'usuarios'
-ORDER BY ordinal_position;
